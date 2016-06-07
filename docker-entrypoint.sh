e4f592f8da868a1dfeec9413adcffcfcc8bfd4e4
Dan Skadra <dskadra@gmail.com>
2016-06-07 00:51:16 -0400

    move configuration before initialization

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,29 +12,31 @@ if [ $1 = "/usr/sbin/init" ]; then
   ## https://tickets.puppetlabs.com/browse/SERVER-441
   mkdir -p /run/puppetlabs
 
-  ## Only initalize and setup the environments (via r10k) if server is launching
-  ##    for the first time (i.e. new server container). We don't want to unintentionally
-  ##    upgrade an environment or break certs on a container restart or upgrade.
-  if [ ! -d  /etc/puppetlabs/puppet/ssl ]; then
-    # Apply inital config on startup.
-    puppet agent --verbose --no-daemonize --onetime --noop \
-            --environment=${PUPPETENV} \
-            --server=${PUPPETSERVER} \
-            --waitforcert=${WAITFORCERT}
-
-    # /opt/puppetlabs/bin/puppetdb ssl-setup -f
-    puppetdb ssl-setup -f
-  fi
-
   ## Set puppet.conf settings
+  puppet config set server ${PUPPETSERVER} --section main --environment production
+  puppet config set environment ${PUPPETENV} --section main --environment production
   puppet config set runinterval ${RUNINTERVAL} --section agent --environment production
   puppet config set waitforcert ${WAITFORCERT} --section agent --environment production
-  puppet config set server ${PUPPETSERVER} --section main --environment production
   puppet config set trusted_server_facts true --section main --environment production
   if [ -v DNSALTNAMES ]; then
     puppet config set dns_alt_names ${DNSALTNAMES} --section main  --environment production
   fi
-  puppet config set environment ${PUPPETENV} --section main --environment production
+
+  ## Only initalize and setup the environments (via r10k) if server is launching
+  ##    for the first time (i.e. new server container). We don't want to unintentionally
+  ##    upgrade an environment or break certs on a container restart or upgrade.
+  if [ ! -d  /etc/puppetlabs/puppet/ssl ]; then
+    # Puppet service isn't running yet, so only setup SSL now to prevent any errors
+    #   puppet runs
+    puppet agent --verbose --no-daemonize --onetime --noop
+            # --environment=${PUPPETENV} \
+            # --server=${PUPPETSERVER} \
+            # --waitforcert=${WAITFORCERT}
+
+    # Configure puppetdb ssl with newley setup puppet ssl
+    puppetdb ssl-setup -f
+  fi
+
 fi
 
 ## Pass control on to the command suppled on the CMD line of the Dockerfile
