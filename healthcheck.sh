f6a4ebdabffb1486e4bddc91676194a01213069e
Dan Skadra <dskadra@gmail.com>
2018-10-31 19:09:11 -0400

    Clean up errors

--- healthcheck.sh
+++ healthcheck.sh
@@ -7,6 +7,6 @@ curl -sS --fail -H 'Accept: pson' \
   --cert   /etc/puppetlabs/puppet/ssl/certs/${certname}.pem \
   --key    /etc/puppetlabs/puppet/ssl/private_keys/${certname}.pem \
   --cacert /etc/puppetlabs/puppet/ssl/certs/ca.pem \
-  https://${certname}:8081/status/v1/services/puppetdb-status \
-  | grep -q '"state":"running"' \
-  || exit 1
+  https://${certname}:8081/status/v1/services/puppetdb-status | \
+  grep -q '"state":"running"' || \
+  exit 1
